f0ee8f2fda20c4f9bb5302bc05809b90b242decd
Bekzod77777 <bekzodjumayev205@gmail.com>
2024-08-22 14:15:43 +0500

    heroku

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ const App = () => {
     const queryID = telegram.initDataUnsafe?.query_id;
 
     if (queryID) {
-      fetch("http://localhost:8000/web-data", {
+      fetch("https://jbbcorptelegrambot-d1f66cd82ce2.herokuapp.com/web-data", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
